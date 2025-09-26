f141845b4da34ccb5e78279e78b5d4db48951d17
voidcord <voidcord54@gmail.com>
2025-09-26 15:11:28 +0800

    init

---
+++ configuration.nix
@@ -0,0 +1,100 @@
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ 
+      ./hardware-configuration.nix
+    ];
+    
+  # Enable flake
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Singapore";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_SG.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_SG.UTF-8";
+    LC_IDENTIFICATION = "en_SG.UTF-8";
+    LC_MEASUREMENT = "en_SG.UTF-8";
+    LC_MONETARY = "en_SG.UTF-8";
+    LC_NAME = "en_SG.UTF-8";
+    LC_NUMERIC = "en_SG.UTF-8";
+    LC_PAPER = "en_SG.UTF-8";
+    LC_TELEPHONE = "en_SG.UTF-8";
+    LC_TIME = "en_SG.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.future = {
+    isNormalUser = true;
+    description = "future";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    ];
+  };
+  
+
+  # Enable automatic login for the user.
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "future";
+
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  	firefox
+  	git
+  	vscodium
+  ];
+
+
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
