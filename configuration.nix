33657618f723dfad6d70069b9da267df329bdff0
voidcord <voidcord54@gmail.com>
2025-09-27 20:15:46 +0800

    change to kde plasma

--- configuration.nix
+++ configuration.nix
@@ -8,11 +8,12 @@
 
       ./modules/apps.nix
       ./modules/boot.nix
-      ./modules/desktop.nix
       ./modules/networking.nix
       ./modules/sound.nix
       ./modules/time.nix
       ./modules/amd.nix
+      ./modules/kde.nix
+
 
       ./modules/users/future.nix
     ];
@@ -22,12 +23,6 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-  
-  # Enable the X11 windowing system.
-  
-  services.xserver.enable = true;
-
-  programs.zsh.enable = true;
 
   system.stateVersion = "25.05"; # Did you read the comment?
 
---
+++ modules/kde.nix
@@ -0,0 +1,6 @@
+{ config, pkgs, ... }:
+
+{
+    services.displayManager.sddm.enable = true;
+    services.desktopManager.plasma6.enable = true; 
+}
