48d2fded6544d6921ebfbe8d21c04dcc4daa587d
voidcord <voidcord54@gmail.com>
2025-09-27 20:16:39 +0800

    change to stable branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Voidcord flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     
